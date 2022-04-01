c1c822319611323054f03a190c20e5e387b63e9a
하상재 <bindongx2@naver.com>
2022-04-01 16:47:17 +0900

    windowOpenView Delegate 설정 변경

--- QR-Code_Objectjve-C/ViewClass/WindowOpenViewController.m
+++ QR-Code_Objectjve-C/ViewClass/WindowOpenViewController.m
@@ -23,19 +23,17 @@
     [super viewDidLoad];
     
     NSLog(@"windowOpenController 진입 : URL[%@]", _requestURL);
-    
-    
-     // 웹뷰의 딜리게이트들을 새로 초기화해줍니다.
-     [self.wkWebView setUIDelegate:self];
-     [self.wkWebView setNavigationDelegate:self];
         
-     CGRect frame = [[UIScreen mainScreen]bounds];
-     // WkWebView는 IBOutlet으로 제공되지 않아 스토리보드에서 추가할 수 없습니다.
-     // 웹뷰의 크기를 정해준 후 초기화하고 본 ViewController의 뷰에 추가합니다.
+    CGRect frame = [[UIScreen mainScreen]bounds];
+    // WkWebView는 IBOutlet으로 제공되지 않아 스토리보드에서 추가할 수 없습니다.
+    // 웹뷰의 크기를 정해준 후 초기화하고 본 ViewController의 뷰에 추가합니다.
 
-     self.wkWebView = [[WKWebView alloc] initWithFrame:frame];
-     [self.wkWebView setNavigationDelegate:self];
-     [self.view addSubview:self.wkWebView];
+    self.wkWebView = [[WKWebView alloc] initWithFrame:frame];
+    
+    // 웹뷰의 딜리게이트들을 새로 초기화해줍니다.
+    [self.wkWebView setUIDelegate:self];
+    [self.wkWebView setNavigationDelegate:self];
+    [self.view addSubview:self.wkWebView];
  
     NSURLRequest *request = [[NSURLRequest alloc] initWithURL:_requestURL];
     [self.wkWebView loadRequest:request];
